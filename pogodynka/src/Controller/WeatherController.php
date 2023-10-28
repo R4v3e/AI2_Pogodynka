67a571a3eb8252d1459ecc1a61a80a0100648b4c
R4v3e <feanor181@outlook.com>
2023-10-28 17:42:50 +0200

    zmiana bazy danych. Dodanie nowych entity do bazy. Stworzenie kontrolera. Uzupelnienie templatow. Wyswietlanie pogody na stronie

---
+++ pogodynka/src/Controller/WeatherController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\LocationRepository; 
+use App\Entity\Location;
+use App\Repository\ForecastRepository;
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{city}', name: 'app_weather', requirements: ['city' => '[a-zA-Z]+'])]
+    public function city(string $city, LocationRepository $locationRepository, ForecastRepository $repository): Response
+    {
+        $location = $locationRepository->findOneBy(['city' => $city]);
+        if (!$location) {
+            $location = $locationRepository->findOneBy(['country' => $city]);
+            if (!$location) {
+                throw $this->createNotFoundException('Location not found for the given city.');
+            }
+        }
+        $measurements = $repository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+}
---
+++ pogodynka/src/Entity/Forecast.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ForecastRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ForecastRepository::class)]
+class Forecast
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 7, scale: 2)]
+    private ?string $windSpeed = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $temperature = null;
+
+    #[ORM\Column]
+    private ?int $cloudiness = null;
+
+    #[ORM\ManyToOne(inversedBy: 'forecasts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $timestamp = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWindSpeed(): ?string
+    {
+        return $this->windSpeed;
+    }
+
+    public function setWindSpeed(string $windSpeed): static
+    {
+        $this->windSpeed = $windSpeed;
+
+        return $this;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(string $temperature): static
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getCloudiness(): ?int
+    {
+        return $this->cloudiness;
+    }
+
+    public function setCloudiness(int $cloudiness): static
+    {
+        $this->cloudiness = $cloudiness;
+
+        return $this;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?\DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(\DateTimeInterface $timestamp): static
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+}
